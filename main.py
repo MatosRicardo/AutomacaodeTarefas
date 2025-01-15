8fde2f06e4c5d7548c4bf0d5668199fcb6ec3160
MatosRicardo <ricardoribeirodematos@gmail.com>
2025-01-15 10:12:44 -0300

    Instalação pyautogui

---
+++ main.py
@@ -0,0 +1,10 @@
+import pyautogui
+
+#pyautogui.click -> clicar
+#pyautogui.write -> escrever
+#pyautogui.press -> pressionar tecla
+#pyautogui.hotkey -> atalho
+
+pyautogui.press("win")
+pyautogui.write("Google chrome")
+pyautogui.press("enter")

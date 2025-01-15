07fd04942e5ffdb9777e7e68fb3ab002c495da13
MatosRicardo <ricardoribeirodematos@gmail.com>
2025-01-15 10:48:36 -0300

    Passo 2: Fazer Login

---
+++ auxiliar.py
@@ -0,0 +1,6 @@
+import pyautogui
+import time
+
+time.sleep(5)
+# Posição de onde está o mouse
+print(pyautogui.position())
--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 import pyautogui
+import time
 
 #pyautogui.click -> clicar
 #pyautogui.write -> escrever
 #pyautogui.press -> pressionar tecla
 #pyautogui.hotkey -> atalho
 
+# Passo 1: Acessar o sistema
+
+# Colocar uma pausa entre uma ação e outra
+pyautogui.PAUSE = 0.5
+# Abrir o menu do Windows
 pyautogui.press("win")
-pyautogui.write("Google chrome")
+
+# Abrir o navegador Opera
+pyautogui.write("Opera")
+
+# Abrir o navegador
+pyautogui.press("enter")
+
+#Digita na barra de navageção
+
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+
+# Pedir pro computador esperar 3 segundos
+time.sleep(3)
+
+# Passo 2: Fazer Login
+pyautogui.click(x=670, y=391)
+
+pyautogui.write("dragonballzgtx32@gmail.com")
+pyautogui.press("tab")
+pyautogui.write("123456")
 pyautogui.press("enter")
